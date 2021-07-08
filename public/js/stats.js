988373a5a24d3b7cf57f370192dcd2f2a41bfd10
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-07-08 10:48:24 -0400

    reverted to previous version of stats.js and cleaned up old commented code

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,25 +1,3 @@
-fetch("/api/workouts/range")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    populateChart(data);
-  });
-
-  API.getWorkoutsInRange()
-
-function duration(data) {
-  let durations = [];
-
-  data.forEach(workout => {
-    workout.exercises.forEach(exercise => {
-      durations.push(exercise.duration);
-    });
-  });
-
-  return durations;
-}
-
 function calculateTotalWeight(data) {
   const totals = [];
 
@@ -35,6 +13,18 @@ function calculateTotalWeight(data) {
   });
 
   return totals;
+}
+
+function duration(data) {
+  let durations = [];
+
+  data.forEach(workout => {
+    workout.exercises.forEach(exercise => {
+      durations.push(exercise.duration);
+    });
+  });
+
+  return durations;
 }
 
 function populateChart(data) {
@@ -130,142 +120,7 @@ function populateChart(data) {
 }
 
 // get all workout data from back-end
-// API.getWorkoutsInRange().then(populateChart);
+API.getWorkoutsInRange().then(populateChart);
 
 
-
-
-
-// NEW
-
-// fetch("/api/workouts/range")
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     populateChart(data);
-//   });
-
-//   API.getWorkoutsInRange().then(populateChart);
-
-
-// function populateChart(data) {
-//   let durations = duration(data);
-//   let pounds = calculateTotalWeight(data);
-//   let workouts = workoutNames(data);
-//   const colors = generatePalette();
-
-//   let line = document.querySelector("#canvas").getContext("2d");
-//   let bar = document.querySelector("#canvas2").getContext("2d");
-
-
-// let lineChart = new Chart(line, {
-//   type: 'line',
-//   data: {
-//     labels,
-//     datasets: [
-//       {
-//         label: 'Workout Duration In Minutes',
-//         backgroundColor: 'red',
-//         borderColor: 'red',
-//         data: durations,
-//         fill: false,
-//       },
-//     ],
-//   },
-//   options: {
-//     responsive: true,
-//     title: {
-//       display: true,
-//       text: 'Time Spent Working Out (Last 7 days)',
-//     },
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   },
-// });
-
-// let barChart = new Chart(bar, {
-//   type: 'bar',
-//   data: {
-//     labels,
-//     datasets: [
-//       {
-//         label: 'Pounds',
-//         data: pounds,
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//           'rgba(54, 162, 235, 0.2)',
-//           'rgba(255, 206, 86, 0.2)',
-//           'rgba(75, 192, 192, 0.2)',
-//           'rgba(153, 102, 255, 0.2)',
-//           'rgba(255, 159, 64, 0.2)',
-//         ],
-//         borderColor: [
-//           'rgba(255, 99, 132, 1)',
-//           'rgba(54, 162, 235, 1)',
-//           'rgba(255, 206, 86, 1)',
-//           'rgba(75, 192, 192, 1)',
-//           'rgba(153, 102, 255, 1)',
-//           'rgba(255, 159, 64, 1)',
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     title: {
-//       display: true,
-//       text: 'Pounds Lifted (Last 7 days)',
-//     },
-//     scales: {
-//       yAxes: [
-//         {
-//           ticks: {
-//             beginAtZero: true,
-//           },
-//         },
-//       ],
-//     },
-//   },
-// });
-
-
-// function duration(data) {
-//   let durations = [];
-
-//   data.forEach(workout => {
-//     workout.exercises.forEach(exercise => {
-//       durations.push(exercise.duration);
-//     });
-//   });
-
-//   return durations;
-// }
-
-// function calculateTotalWeight(data) {
-//   let total = [];
-
-//   data.forEach(workout => {
-//     workout.exercises.forEach(exercise => {
-//       total.push(exercise.weight);
-//     });
-//   });
-
-//   return total;
-// }
-
-// function workoutNames(data) {
-//   let workouts = [];
-
-//   data.forEach(workout => {
-//     workout.exercises.forEach(exercise => {
-//       workouts.push(exercise.name);
-//     });
-//   });
-  
-//   return workouts;
-// }};
 
